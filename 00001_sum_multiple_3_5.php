1ccf28f9103085437541d98ea9a3d0846d53d604
Dipak Yadav <dipakmdhrm@gmail.com>
2018-05-24 09:28:58 +0530

    Problem 1, Solution 2.

--- 00001_sum_multiple_3_5.php
+++ 00001_sum_multiple_3_5.php
@@ -2,23 +2,24 @@
 
 /**
  * Calculate sum or all natural numbers below $n that are multiple of 3 or 5.
- * Time Complexity: O(N).
+ * Time Complexity: O(N/3 + N/5).
+ * 
  * @param int $n
  *   
  * @return int
  */
 function sum_multiple_3_5($n) {
-  $sum = 0;
-
-  for ($i = 1; $i < $n; $i++) { 
-    if ($i % 3 == 0 ) {
-      $sum += $i;
-    }    
-    elseif ($i % 5 == 0) {
-      $sum += $i;
+  $sum3 = 0;
+  $sum5 = 0;
+  for ($i = 3; $i < $n; $i += 3) { 
+    $sum3 += $i;
+  }
+  for ($i = 5; $i < $n; $i += 5) { 
+    if ($i % 3 !== 0) {
+      $sum5 += $i;
     }
   }
-  return $sum;
+  return $sum3 + $sum5;
 }
 
 echo sum_multiple_3_5(1000);
