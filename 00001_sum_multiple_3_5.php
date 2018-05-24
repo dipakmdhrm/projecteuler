74569ccd755c091556e483afb56ae0b18a98e62a
Dipak Yadav <dipakmdhrm@gmail.com>
2018-05-24 09:20:15 +0530

    Initial commit: Euler sol 1.

---
+++ 00001_sum_multiple_3_5.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Calculate sum or all natural numbers below $n that are multiple of 3 or 5.
+ *
+ * @param int $n
+ *   
+ * @return int
+ */
+function sum_multiple_3_5($n) {
+  $sum = 0;
+
+  for ($i = 1; $i < $n; $i++) { 
+    if ($i % 3 == 0 ) {
+      $sum += $i;
+    }    
+    elseif ($i % 5 == 0) {
+      $sum += $i;
+    }
+  }
+  return $sum;
+}
+
+echo sum_multiple_3_5(1000);
