e57887a09ad92b87ad09fbb1081d4be3e8ce63d9
Dipak Yadav <dipakmdhrm@gmail.com>
2018-05-24 09:39:27 +0530

    Problem 2, Solution 1.

---
+++ 00002_even_fibonacci_numbers.php
@@ -0,0 +1,19 @@
+<?php
+
+function sum_even_fibonacci($n) {
+  $fib1 = 1;
+  $fib2 = 2;
+  $sum = $fib2;
+  while ($fib2 <= $n) {
+    $temp = $fib1;
+    $fib1 = $fib2;
+    $fib2 = $temp + $fib1;
+    if ($fib2 % 2 === 0) {
+      $sum += $fib2;
+    }
+  }
+
+  return $sum;
+}
+
+echo sum_even_fibonacci(4000000);
